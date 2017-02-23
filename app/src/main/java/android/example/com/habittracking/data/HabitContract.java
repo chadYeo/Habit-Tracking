ca952eb30142d5b4e4534f089b20ac78fbbe8788
Chad Yeo <chad.yeo.cy@gmail.com>
2017-02-23 17:57:28 -0500

    HabitContract is finished

--- app/src/main/java/android/example/com/habittracking/data/HabitContract.java
+++ app/src/main/java/android/example/com/habittracking/data/HabitContract.java
@@ -1,5 +1,17 @@
 package android.example.com.habittracking.data;
 
 
+import android.provider.BaseColumns;
+
 public class HabitContract {
+
+    private HabitContract() {
+    };
+
+    public static class HabitEntry implements BaseColumns {
+        public static final String TABLE_NAME = "habit";
+        public static final String COLUMN_HABIT_NAME = "name";
+        public static final String COLUMN_HABIT_DATE = "date";
+        public static final String COLUMN_HABIT_NUMBER = "number";
+    }
 }
