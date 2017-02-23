e08697e9d72d2140025b35e97449108e3480faeb
Chad Yeo <chad.yeo.cy@gmail.com>
2017-02-23 14:28:13 -0500

    onCancelButtonClicked method is added in AddhabitActivity

--- app/src/main/java/android/example/com/habittracking/AddhabitActivity.java
+++ app/src/main/java/android/example/com/habittracking/AddhabitActivity.java
@@ -1,7 +1,9 @@
 package android.example.com.habittracking;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AddhabitActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AddhabitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addhabit);
+    }
+
+    public void onCancelButtonClicked(View v) {
+        Intent intent = new Intent(AddhabitActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
